fa53163e127334e1dea033f3af2e55b6190397fe
Anggun Permata <anggunpermata.g789@gmail.com>
2021-11-30 15:25:33 +0700

    CRU actions

---
+++ db/migrate/20211126070217_add_timestamps_to_articles.rb
@@ -0,0 +1,8 @@
+class AddTimestampsToArticles < ActiveRecord::Migration[6.1]
+  def change
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
